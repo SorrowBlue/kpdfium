0a8662e984cb7dbd06cc2fda61ed8b392631da7f
SorrowBlue <sorrowblue.dev@gmail.com>
2026-09-07 01:44:20 +0900

    build: clean up gradle scripts and lint configuration

--- build-logic/src/main/kotlin/kpdfium.lint.gradle.kts
+++ build-logic/src/main/kotlin/kpdfium.lint.gradle.kts
@@ -36,29 +36,12 @@ when {
 }
 
 private fun Lint.configure() {
-    val isCI = System.getenv("CI").toBoolean()
     checkAllWarnings = true
     checkDependencies = true
-//    disable += listOf(
-//        "NewerVersionAvailable",
-//        "GradleDependency",
-//        "AppLinksAutoVerify",
-//    )
+    disable += listOf(
+        "NewerVersionAvailable",
+        "GradleDependency",
+        "AppLinksAutoVerify",
+    )
     baseline = rootProject.file("config/lint-baseline.xml")
-    htmlReport = !isCI
-    htmlOutput =
-        if (htmlReport) {
-            project.file("${project.rootDir}/build/reports/lint/lint-result.html")
-        } else {
-            null
-        }
-    sarifReport = isCI
-    sarifOutput =
-        if (sarifReport) {
-            project.file("${project.rootDir}/build/reports/lint/lint-result.sarif")
-        } else {
-            null
-        }
-    textReport = false
-    xmlReport = false
 }
